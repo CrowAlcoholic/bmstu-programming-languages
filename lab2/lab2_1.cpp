1858f59231f98512af8f6f254fec302bceb63140
Aleksandr Vorona <32168978+CrowAlcoholic@users.noreply.github.com>
2017-09-29 16:52:37 +0300

    Update lab2_1.cpp

--- lab2/lab2_1.cpp
+++ lab2/lab2_1.cpp
@@ -1,72 +1,152 @@
 #include <iostream>
-#include <cmath>
 
-int main()
-{
-    // if
+int main() {
+    // 1 
+    /* std::cout << "Enter two numbers" << std::endl;
+    int x1 = 0;
+    int y1 = 0;
+    std::cin >> x1 >> y1;
+    if ((x1 % 2 == 1) && (y1 % 2 == 1))
     {
-        const int right = 25;
-        // right = 234; // error
-
-        int answer = 0;
-        std::cout << "5 * 5 = ?" << std::endl;
-        std::cin >> answer;
-
-        if (answer == right)
-        {
-            std::cout << "All right!" << std::endl;
-        }
-        else
-        {
-            std::cout << "You're looser" << std::endl;
-        }
+        std::cout << "Both numbers are not even" << std::endl;
     }
-
-    // for
+    else
     {
-        unsigned int n = 0;
-        std::cout << "Enter a number:" << std::endl;
-        std::cin >> n;
-
-        unsigned int fact = 1;
-        for (unsigned int i = 2; i <= n; ++i)
-        {
-            fact = fact * i;
-        }
-
-        std::cout << n << "! = " << fact << std::endl;
+        std::cout << "At least one number is even" << std::endl;
     }
-
-    // while
+    // 2
+    std::cout << "Enter two numbers" << std::endl;
+    int x2 = 0;
+    int y2 = 0;
+    std::cin >> x2 >> y2;
+    if ((x2 < 20) && (y2 >= 20) || ((x2 >= 20) && (y2 < 20)))
     {
-        unsigned int n = 0;
-        std::cout << "Enter a number:" << std::endl;
-        std::cin >> n;
-        std::cout << "Summ(0..." << n << ") = ";
-
-        unsigned int summ = 0;
-        while (n != 0)
-        {
-            summ += n;
-            n--;
-        }
-
-        std::cout << summ << std::endl;
+        std::cout << "Only one number is less than 20" << std::endl;
     }
-
-    // do while
+    else
     {
-        const int right = 25;
-
-        int answer = 0;
-        do
-        {
-            std::cout << "5 * 5 = ?" << std::endl;
-            std::cin >> answer;
-        } while (answer != right);
-
-        std::cout << "All right!" << std::endl;
+        std::cout << "Both numbers are less than 20 or more than 20 " << std::endl;
     }
-
+    // 3
+    std::cout << "Enter two numbers" << std::endl;
+    int x3 = 0;
+    int y3 = 0;
+    std::cin >> x3 >> y3;
+    if ((x3 == 0) || (y3 == 0))
+    {
+        std::cout << "At least one number is 0" << std::endl;
+    }
+    else
+    {
+        std::cout << "Both numbers are not 0" << std::endl;
+    }
+    // 4
+    std::cout << "Enter three numbers" << std::endl;
+    int x4 = 0;
+    int y4 = 0;
+    int z4 = 0;
+    std::cin >> x4 >> y4 >> z4;
+    if ((x4 < 0) && (y4 < 0) && (z4 < 0))
+    {
+        std::cout << "All numbers are negative" << std::endl;
+    }
+    else
+    {
+        std::cout << "At least one number is positive" << std::endl;
+    }
+    //  5
+    std::cout << "Enter three numbers" << std::endl;
+    int x5 = 0;
+    int y5 = 0;
+    int z5 = 0;
+    std::cin >> x5 >> y5 >> z5;
+    if ((x5 % 5 == 0) && (y5 % 5 != 0) && (z5 % 5 != 0))
+    {
+        std::cout << "First number multiples of 5" << std::endl;
+    }
+    else if ((x5 % 5 != 0) && (y5 % 5 == 0) && (z5 % 5 != 0))
+    {
+        std::cout << "Second number multiples of 5" << std::endl;
+    }
+    else if ((x5 % 5 != 0) && (y5 % 5 != 0) && (z5 % 5 == 0))
+    {
+        std::cout << "Third number multiples of 5" << std::endl;
+    }
+    else
+    {
+        std::cout << "None numbers multiples 5" << std::endl;
+    }
+    // 6
+    std::cout << "Enter three numbers" << std::endl;
+    int x6 = 0;
+    int y6 = 0;
+    int z6 = 0;
+    std::cin >> x6 >> y6 >> z6;
+    if ((x6 > 100) && (y6 <= 100) && (z6 <= 100))
+    {
+        std::cout << "First number is more than 100" << std::endl;
+    }
+    else if ((x6 <= 100) && (y6 > 100) && (z6 <= 100))
+    {
+        std::cout << "Second number is more than 100" << std::endl;
+    }
+    else if ((x6 <= 100) && (y6 <= 100) && (z6 > 100))
+    {
+        std::cout << "Third number is more than 100" << std::endl;
+    }
+    else
+    {
+        std::cout << "All numbers are less than 100" << std::endl;
+    } */
+    ///////////////////////////////////////////////////////////////////////////////////////// 7
+    std::cout << "enter a,b for rook" << std::endl;
+    std::pair<int, int> rook;
+    std::cin >> rook.first >> rook.second;
+    std::cout << "enter c,d for target" << std::endl;
+    std::pair<int, int> target1;
+    std::cin >> target1.first >> target1.second;
+    while ((0 > rook.first) || (rook.first > 8) || (0 > rook.second) || (rook.second > 8))
+    {
+        std::cout << "a, b must be > 0 and < 8. Enter a,b for rook again" << std::endl;
+        std::cin >> rook.first >> rook.second;
+    }
+    while ((0 > target1.first) || (target1.first > 8) || (0 > target1.second) || (target1.second > 8))
+    {
+        std::cout << "c, d must be > 0 and < 8. Enter c,d for target again" << std::endl;
+        std::cin >> rook.first >> rook.second;
+    }
+    if ((rook.first == target1.first) || (rook.second == target1.second)) {
+    std::cout << "Danger!!! rook can hit " << target1.first << target1.second << std::endl;
+    }
+    else
+    {
+    std::cout << "Chill, rook can't hit " << target1.first << target1.second << std::endl;
+    } 
+    ///////////////////////////////////////////////////////////////////////////////////////// 8
+    std::cout << "enter a,b for bishop" << std::endl;
+    std::pair<int, int> bishop;
+    std::cin >> bishop.first >> bishop.second;
+    std::cout << "enter c,d for second figure " << std::endl;
+    std::pair<int, int> target2;
+    std::cin >> target2.first >> target2.second;
+    while ((0 > bishop.first) || (bishop.first > 8) || (0 > bishop.second) || (bishop.second > 8))
+    {
+        std::cout << "a must be > 0 and < 8. Enter a,b for bishop again" << std::endl;
+        std::cin >> rook.first >> rook.second;
+    }
+    while ((0 > target2.first) || (target2.first > 8) || (0 > target2.second) || (target2.second > 8))
+    {
+        std::cout << "b must be > 0 and < 8. Enter c,d for target again" << std::endl;
+        std::cin >> rook.first >> rook.second;
+    }
+    if ((bishop.first - target2.second) == (bishop.second - target2.first) 
+        || (bishop.first - target2.second) == -(bishop.second - target2.first)) {
+        std::cout << "Danger!!! bishop can hit " << target2.first << target2.second << std::endl;
+    }
+    else
+    {
+        std::cout << "Chill, bishop can't hit " << target2.first << target2.second << std::endl;
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////// 9
     return 0;
 }
